bdfd90c8c3fa94583b2a9eb36f94c2c4202f13c2
jisooyoon <ygs12567@gmail.com>
2023-04-16 20:58:04 +0900

    useLocalStorage logic

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './css/Reset.css';
 import './css/index.css';
 import ToDo from './components/ToDo';
+import { DarkModeProvider } from './context/DarkModeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ToDo />
+    <DarkModeProvider>
+      <ToDo />
+    </DarkModeProvider>
   </React.StrictMode>
 );
